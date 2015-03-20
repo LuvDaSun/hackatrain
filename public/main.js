31993ec6a0823c29acc0e3f33cc70723bd3d7abc
Elmer Bulthuis <elmerbulthuis@gmail.com>
2015-03-20 22:45:45 +0000

    developing

--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
 window.addEventListener('load', function() {
-    var trainElement = document.getElementById('train');
+    var slice = Array.prototype.slice;
+    var trainElements = slice.apply(document.getElementsByClassName('train'));
     var animationFrame;
     window.addEventListener('scroll', function(e) {
         window.cancelAnimationFrame(animationFrame);
         animationFrame = window.requestAnimationFrame(function() {
             //document.getElementById('train').style.transform = 'translate3d(0,' + window.pageYOffset + 'px,0)';
-            trainElement.style.top = (window.pageYOffset - 150) + 'px';
+            trainElements.forEach(function(trainElement) {
+                trainElement.style.top = (window.pageYOffset - 1000) + 'px';
+            });
         });
     });
 });
