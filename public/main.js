827f3f7614ab332b5eb2775608587ab7cbf36dc1
Elmer Bulthuis <elmerbulthuis@gmail.com>
2015-03-21 11:56:35 +0000

    animate train

--- public/main.js
+++ public/main.js
@@ -1,12 +1,31 @@
 /* jshint browser */
 
 window.addEventListener('load', function() {
+    var railsElement = document.getElementById('rails');
     var trainElement = document.getElementById('train');
     var animationFrame;
-    window.addEventListener('scroll', function(e) {
+    var trainRange = railsElement.offsetHeight;
+    var trainOffset = 0 - trainElement.offsetHeight;
+
+    window.addEventListener('scroll', move);
+    window.addEventListener('resize', move);
+
+    function move(e) {
         window.cancelAnimationFrame(animationFrame);
         animationFrame = window.requestAnimationFrame(function() {
-            trainElement.style.top = (window.pageYOffset - 800) + 'px';
+            var scrollOffset = railsElement.offsetTop + window.innerHeight / 2 - trainOffset / 2;
+            var scrollHeight = railsElement.offsetHeight;
+
+            if (window.pageYOffset < scrollOffset) {
+                trainElement.style.top = trainOffset + 'px';
+            }
+            else if (window.pageYOffset < scrollOffset + scrollHeight) {
+                trainElement.style.top = (trainOffset + trainRange * Math.sin(Math.PI * (window.pageYOffset - scrollOffset) / scrollHeight / 2)) + 'px';
+            }
+            else {
+                trainElement.style.top = (trainOffset + trainRange) + 'px';
+            }
         });
-    });
+    }
+
 });
