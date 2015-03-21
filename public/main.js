1c9742f29862ba5c630b414148ee8b691b165d1f
Elmer Bulthuis <elmerbulthuis@gmail.com>
2015-03-21 10:27:02 +0000

    ids

--- public/main.js
+++ public/main.js
@@ -1,16 +1,12 @@
 /* jshint browser */
 
 window.addEventListener('load', function() {
-    var slice = Array.prototype.slice;
-    var trainElements = slice.apply(document.getElementsByClassName('train'));
+    var trainElement = document.getElementById('train');
     var animationFrame;
     window.addEventListener('scroll', function(e) {
         window.cancelAnimationFrame(animationFrame);
         animationFrame = window.requestAnimationFrame(function() {
-            //document.getElementById('train').style.transform = 'translate3d(0,' + window.pageYOffset + 'px,0)';
-            trainElements.forEach(function(trainElement) {
-                trainElement.style.top = (window.pageYOffset - 1000) + 'px';
-            });
+            trainElement.style.top = (window.pageYOffset - 1000) + 'px';
         });
     });
 });
