5b696df218c46d7124aa6af8833ff9d129ef72ac
Elmer Bulthuis <elmerbulthuis@gmail.com>
2015-03-21 12:58:50 +0000

    optimize train

--- public/main.js
+++ public/main.js
@@ -1,31 +1,46 @@
 /* jshint browser */
 
+
+
 window.addEventListener('load', function() {
     var trackElement = document.getElementById('track');
     var railsElement = document.getElementById('rails');
     var trainElement = document.getElementById('train');
-    var animationFrame;
     var trainRange = railsElement.offsetHeight;
     var trainOffset = 0 - trainElement.offsetHeight;
 
     window.addEventListener('scroll', move);
     window.addEventListener('resize', move);
+    move();
 
-    function move(e) {
+    var animationFrame;
+
+    function move() {
         window.cancelAnimationFrame(animationFrame);
         animationFrame = window.requestAnimationFrame(function() {
             var scrollOffset = trackElement.offsetTop - window.innerHeight / 2;
             var scrollHeight = trackElement.offsetHeight;
+            var top;
 
             if (window.pageYOffset < scrollOffset) {
-                trainElement.style.top = trainOffset + 'px';
+                top = 0;
             }
             else if (window.pageYOffset < scrollOffset + scrollHeight) {
-                //trainElement.style.top = (trainOffset + trainRange * (window.pageYOffset - scrollOffset) / scrollHeight) + 'px';
-                trainElement.style.top = (trainOffset + trainRange * Math.sin(Math.PI * (window.pageYOffset - scrollOffset) / scrollHeight / 2)) + 'px';
+                //top = trainRange * (window.pageYOffset - scrollOffset) / scrollHeight;
+                top = trainRange * Math.sin(Math.PI * (window.pageYOffset - scrollOffset) / scrollHeight / 2);
             }
             else {
-                trainElement.style.top = (trainOffset + trainRange) + 'px';
+                top = trainRange;
+            }
+
+            if (top > trainElement.offsetHeight) {
+                trainElement.style.top = '';
+                trainElement.style.transform = 'translate3d(0,' + (trainOffset + top) + 'px,0)';
+            }
+            else {
+                trainElement.style.top = (trainOffset + top) + 'px';
+                trainElement.style.transform = '';
+                //trainElement.style.transform = 'translateY(' + (trainOffset + top) + 'px)';
             }
         });
     }
