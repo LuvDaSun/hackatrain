20592c1fa8b40a5bfa4f7db1d2c85f4617bb6f2a
Elmer Bulthuis <elmerbulthuis@gmail.com>
2015-03-22 17:07:09 +0000

    use modernizr

--- public/train.js
+++ public/train.js
@@ -2,7 +2,7 @@
 
 
 jQuery(function($) {
-
+    var Modernizr = window.Modernizr;
     var $window = $(window);
     var $track = $('#track');
     var $rails = $('#rails');
@@ -11,6 +11,25 @@ jQuery(function($) {
     var trainRange = $rails.height();
     var trainHeight = $train.height();
     var trainOffset = 0 - trainHeight;
+
+    var move = Modernizr.csstransforms && Modernizr.csstransforms3d ? function(top) {
+        if (top > trainHeight) {
+            $train.css({
+                transform: 'translate3d(0,' + (trainOffset + top) + 'px,0)',
+            });
+        }
+        else {
+            $train.css({
+                transform: 'translate(0,' + (trainOffset + top) + 'px)',
+            });
+
+        }
+    } : function(top) {
+        $train.css({
+            top: (trainOffset + top) + 'px',
+        });
+    };
+
 
     $window.bind('scroll', animate);
     $window.bind('resize', animate);
@@ -22,32 +41,21 @@ jQuery(function($) {
         var windowHeight = $window.height();
         var scrollOffset = trackPosition.top - windowHeight * 1 / 4;
         var scrollHeight = $track.height() - windowHeight * 2 / 4;
-        var top;
 
         if (window.pageYOffset < scrollOffset) {
-            top = 0;
+            move(0);
         }
         else if (window.pageYOffset < scrollOffset + scrollHeight) {
-            //top = trainRange * (window.pageYOffset - scrollOffset) / scrollHeight;
-            top = trainRange * Math.sin(Math.PI * (window.pageYOffset - scrollOffset) / scrollHeight / 2);
+            //move(trainRange * (window.pageYOffset - scrollOffset) / scrollHeight);
+            move(trainRange * Math.sin(Math.PI * (window.pageYOffset - scrollOffset) / scrollHeight / 2));
         }
         else {
-            top = trainRange;
+            move(trainRange);
         }
 
-        if (top > trainHeight) {
-            $train.css({
-                top: '',
-                transform: 'translate3d(0,' + (trainOffset + top) + 'px,0)',
-            });
-        }
-        else {
-            $train.css({
-                top: (trainOffset + top) + 'px',
-                transform: '',
-                //transform: 'translateY(' + (trainOffset + top) + 'px)',
-            });
-        }
+
     }
+
+
 
 });
